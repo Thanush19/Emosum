ad380d0196d1148c10f43440ad88bf782b8f84cf
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-02-26 14:46:47 +0530

    msg_sentiment endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,69 +1,110 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 import { getReceiverSocketId, io } from "../socket/socket.js";
+import axios from "axios";
 
 export const sendMessage = async (req, res) => {
-	try {
-		const { message } = req.body;
-		const { id: receiverId } = req.params;
-		const senderId = req.user._id;
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-		let conversation = await Conversation.findOne({
-			participants: { $all: [senderId, receiverId] },
-		});
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-		if (!conversation) {
-			conversation = await Conversation.create({
-				participants: [senderId, receiverId],
-			});
-		}
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
 
-		const newMessage = new Message({
-			senderId,
-			receiverId,
-			message,
-		});
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
 
-		if (newMessage) {
-			conversation.messages.push(newMessage._id);
-		}
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
 
-		// await conversation.save();
-		// await newMessage.save();
+    // await conversation.save();
+    // await newMessage.save();
 
-		// this will run in parallel
-		await Promise.all([conversation.save(), newMessage.save()]);
+    // this will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
 
-		// SOCKET IO FUNCTIONALITY WILL GO HERE
-		const receiverSocketId = getReceiverSocketId(receiverId);
-		if (receiverSocketId) {
-			// io.to(<socket_id>).emit() used to send events to specific client
-			io.to(receiverSocketId).emit("newMessage", newMessage);
-		}
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
-		res.status(201).json(newMessage);
-	} catch (error) {
-		console.log("Error in sendMessage controller: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
-	}
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export const getMessages = async (req, res) => {
-	try {
-		const { id: userToChatId } = req.params;
-		const senderId = req.user._id;
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
 
-		const conversation = await Conversation.findOne({
-			participants: { $all: [senderId, userToChatId] },
-		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
 
-		if (!conversation) return res.status(200).json([]);
+    if (!conversation) return res.status(200).json([]);
 
-		const messages = conversation.messages;
+    const messages = conversation.messages;
 
-		res.status(200).json(messages);
-	} catch (error) {
-		console.log("Error in getMessages controller: ", error.message);
-		res.status(500).json({ error: "Internal server error" });
-	}
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+export const msg_sentiment = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    // Extract messages from conversation
+    const messages = conversation.messages.map((message) => message.message);
+
+    // Define options for sentiment analysis API
+    const options = {
+      method: "POST",
+      url: "https://sentiment-analysis9.p.rapidapi.com/sentiment",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+        "X-RapidAPI-Key": "b65354c2edmsh784f0298ff419c3p113eebjsncdfd0326168d",
+        "X-RapidAPI-Host": "sentiment-analysis9.p.rapidapi.com",
+      },
+      data: messages.map((message, index) => ({
+        id: index + 1,
+        language: "en",
+        text: message,
+      })),
+    };
+
+    // Call sentiment analysis API
+    const response = await axios.request(options);
+    console.log(response.data);
+
+    // Return sentiment analysis results
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.log("Error in getMessages controller: ", err.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getMessages, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  sendMessage,
+  msg_sentiment,
+} from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
+router.post("/msg_sentiment/:id", protectRoute, msg_sentiment);
 
 export default router;
